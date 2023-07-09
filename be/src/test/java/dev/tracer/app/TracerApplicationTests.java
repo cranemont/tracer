672db47e6ed171ce4e1fe076a16472d7037582b7
cranemont <cranemont@gmail.com>
2023-07-09 16:27:54 +0900

    init

---
+++ be/src/test/java/dev/tracer/app/TracerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tracer.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
